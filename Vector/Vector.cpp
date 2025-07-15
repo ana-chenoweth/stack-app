7f1b696aac9f3779cf414048eef70354cce7ea92
Ana Laura Chenoweth Galaz <analaura@Anas-MacBook-Air.local>
2025-07-15 10:53:31 -0400

    Constructor de copias

---
+++ Vector/Vector.cpp
@@ -0,0 +1,28 @@
+#include <math.h>
+#include "Vector.hpp"
+//***********************************
+Vector::Vector()
+{
+ EstablecerDim(dim);
+ try
+     {
+         for(int i = 0; i < dim; ++i){
+             componente[i] = 0;
+         }
+     }catch(std::bad_alloc &){
+         throw "No es posible construir un Vector";
+     }
+}
+//***********************************
+Vector::Vector(const Vector &v)
+{
+    EstablecerDim(v.dim);
+    try
+    {
+        for(int i = 0; i < dim; ++i){
+            componente[i] = v.componente[i];
+        }
+    }catch(std::bad_alloc &){
+         throw "No es posible construir un Vector";
+    }
+}
---
+++ Vector/Vector.hpp
@@ -0,0 +1,22 @@
+#ifndef VECTOR_HPP_INCLUDED
+#define VECTOR_HPP_INCLUDED
+
+#include <iostream>
+//Definicion de la clase
+
+const int MAXDIM = 100; // Definimos un tamaño máximo para el vector
+
+
+class Vector{
+
+public:
+    Vector();
+    Vector(const Vector &v);
+private:
+    int dim;
+    double componente[MAXDIM];
+    //metodo de utileria
+    void EstablecerDim(int dim);
+};
+
+#endif // VECTOR_HPP_INCLUDED
